5af98047c75603f6b37b4f3678ec90c2ed5c8018
Chuvaaaaaaak <mynamme1@gmail.com>
2021-03-02 22:19:17 +0300

    Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,15 +1,23 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        Util util = new Util();
-        util.getConnection();
+        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+
+        userDaoHibernate.dropUsersTable();
+        userDaoHibernate.createUsersTable();
+
+        userDaoHibernate.saveUser("test", "fgf", (byte) 12);
+        userDaoHibernate.saveUser("test", "fgf", (byte) 12);
+
 
     }
 }
